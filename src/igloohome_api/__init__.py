cd228d3595b58c51e637c32fe2bb53ea3e312256
Keith <22891515+keithle888@users.noreply.github.com>
2026-07-19 07:31:42 +0200

    Add GetDeviceInfoResponse.lastSync field (#6)
    # Added
    - GetDeviceInfoResponse.lastSync as an optional datetime field.
    - Added global dacite config to ensure that it can deserialize ISO8601 datetime to datetime type
    
    # Changed
    - Version bump 0.1.2
    
    closes #5

--- src/igloohome_api/__init__.py
+++ src/igloohome_api/__init__.py
@@ -1,7 +1,8 @@
 """Library for accessing igloohome API"""
 from typing import Optional
 
-from dacite import from_dict
+from dacite import from_dict, Config
+from datetime import datetime
 
 import aiohttp
 import jwt
@@ -21,6 +22,17 @@ _BASE_URL = "https://api.igloodeveloper.co"
 _BASE_PATH = "igloohome"
 _DEVICES_PATH_SEGMENT = "devices"
 
+# Helper function to normalize standard ISO formats for Python < 3.11
+def _parse_iso_datetime(val: str) -> datetime:
+    if val.endswith("Z"):
+        val = val[:-1] + "+00:00"
+    return datetime.fromisoformat(val)
+
+
+_GLOBAL_CONFIG = config = Config(type_hooks={
+    datetime: _parse_iso_datetime
+})
+
 
 @dataclass
 class LinkedDevice:
@@ -38,6 +50,8 @@ class GetDeviceInfoResponse:
     homeId: list[str]
     linkedDevices: list[LinkedDevice]
     batteryLevel: Optional[int]
+    lastSync: datetime
+    
 
 
 @dataclass
@@ -166,7 +180,7 @@ class Api:
             f'{self.host}/{_BASE_PATH}/{_DEVICES_PATH_SEGMENT}',
         )
         if response.status == 200:
-            return from_dict(GetDevicesResponse, await response.json())
+            return from_dict(GetDevicesResponse, await response.json(), config=_GLOBAL_CONFIG)
         else:
             raise ApiException("Response failure", response.status)
 
@@ -176,7 +190,7 @@ class Api:
             f'{self.host}/{_BASE_PATH}/{_DEVICES_PATH_SEGMENT}/{deviceId}',
         )
         if response.status == 200:
-            return from_dict(GetDeviceInfoResponse, await response.json())
+            return from_dict(GetDeviceInfoResponse, await response.json(), config=_GLOBAL_CONFIG)
         else:
             raise ApiException("Response failure", response.status)
 
